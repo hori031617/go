221528535ea260db57d360777d4227005bad1246
Masahiro Hori <good.bye.baseball.17@gmail.com>
2016-05-15 17:01:35 +0900

    サンプルコード追加

--- src/helloworld/helloworld.go
+++ src/helloworld/helloworld.go
@@ -2,8 +2,18 @@ package main
 
 import (
     "fmt"
+    "calc"
+    "github.com/bitly/go-simplejson"
 )
 
 func main(){
-    fmt.Println("Hello, World!")
+    fmt.Println(calc.Add(1,2))
+    fmt.Println("\n")
+
+    json := simplejson.New()
+    json.Set("message", "Hello, World!")
+
+    b, _ := json.EncodePretty()
+
+    fmt.Printf("%s\n", b)
 }
